cff968337c9ee775df2f0d169bdd34757ec45693
jagadish <jagadishreddykovvuri@gmail.com>
2019-08-06 14:59:09 +0530

    [Red State]evaluating the expression input

--- src/Store/Store.test.js
+++ src/Store/Store.test.js
@@ -4,7 +4,7 @@ describe("Store Test Cases", () => {
   beforeEach(() => {
     calculatorStore = new CalculatorStore();
     expect(calculatorStore.expressionInput.length).toBe(0);
-    calculatorStore.addCharToExpressionInput(7);
+    calculatorStore.addCharToExpressionInput(6);
     calculatorStore.addCharToExpressionInput("/");
   });
   it("should add character to expression Input", () => {
@@ -15,5 +15,11 @@ describe("Store Test Cases", () => {
     expect(calculatorStore.expressionInput.length).toBe(2);
     calculatorStore.deleteCharFromExpressionInput();
     expect(calculatorStore.expressionInput.length).toBe(1);
+  });
+
+  it("should calculate result from expressionInput", () => {
+    expect(calculatorStore.expressionInput.length).toBe(2);
+    calculatorStore.addCharToExpressionInput(2);
+    expect(calculatorStore.result).toBe(3);
   });
 });
