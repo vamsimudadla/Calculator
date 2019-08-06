2fda09d05d65e35049057582b485840a99c60f74
jagadish <jagadishreddykovvuri@gmail.com>
2019-08-06 14:51:55 +0530

    [Refactored] placed some code in beforeEach in testcases

--- src/Store/Store.test.js
+++ src/Store/Store.test.js
@@ -1,18 +1,17 @@
 import CalculatorStore from "./index";
 describe("Store Test Cases", () => {
-  it("should add character to expression Input", () => {
-    const calculatorStore = new CalculatorStore();
+  let calculatorStore;
+  beforeEach(() => {
+    calculatorStore = new CalculatorStore();
     expect(calculatorStore.expressionInput.length).toBe(0);
     calculatorStore.addCharToExpressionInput(7);
     calculatorStore.addCharToExpressionInput("/");
+  });
+  it("should add character to expression Input", () => {
     expect(calculatorStore.expressionInput.length).toBe(2);
   });
 
   it("should delete last character from expressionInput", () => {
-    const calculatorStore = new CalculatorStore();
-    expect(calculatorStore.expressionInput.length).toBe(0);
-    calculatorStore.addCharToExpressionInput(7);
-    calculatorStore.addCharToExpressionInput("/");
     expect(calculatorStore.expressionInput.length).toBe(2);
     calculatorStore.deleteCharFromExpressionInput();
     expect(calculatorStore.expressionInput.length).toBe(1);
