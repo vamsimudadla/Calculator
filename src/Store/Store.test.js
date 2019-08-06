7f7a60446f56adef0d052c0dacf680865ba2e787
jagadish <jagadishreddykovvuri@gmail.com>
2019-08-06 14:42:52 +0530

    [Red State]deleting last character fromexpression input

--- src/Store/Store.test.js
+++ src/Store/Store.test.js
@@ -7,4 +7,14 @@ describe("Store Test Cases", () => {
     calculatorStore.addCharToExpressionInput("/");
     expect(calculatorStore.expressionInput.length).toBe(2);
   });
+
+  it("should delete last character from expressionInput", () => {
+    const calculatorStore = new CalculatorStore();
+    expect(calculatorStore.expressionInput.length).toBe(0);
+    calculatorStore.addCharToExpressionInput(7);
+    calculatorStore.addCharToExpressionInput("/");
+    expect(calculatorStore.expressionInput.length).toBe(2);
+    calculatorStore.deleteCharFromExpressionInput();
+    expect(calculatorStore.expressionInput.length).toBe(1);
+  });
 });
